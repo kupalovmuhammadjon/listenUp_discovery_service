2ff20699ca44668b9dd2c23b85408aa885cf50bd
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-07-01 14:00:59 +0500

    postgres methods added

--- storage/postgres/episode_metadata.go
+++ storage/postgres/episode_metadata.go
@@ -1,6 +1,10 @@
 package postgres
 
-import "database/sql"
+import (
+	"database/sql"
+	pb "discovery_service/genproto/episode_metadata"
+	"fmt"
+)
 
 type EpisodeMetadataRepo struct {
 	Db *sql.DB
@@ -8,4 +12,34 @@ type EpisodeMetadataRepo struct {
 
 func NewEpisodeMetadataRepo(db *sql.DB) *EpisodeMetadataRepo {
 	return &EpisodeMetadataRepo{Db: db}
+}
+
+func (e *EpisodeMetadataRepo) GetPodcastsByGenre(genres *pb.Genres) ([]string, error) {
+	query := `select podcast_id
+	from episode_metadata
+	where deleted_at = null`
+
+	var params []interface{}
+	for i, v := range genres.Genres {
+		query += fmt.Sprintf(" and genre = $%d", i+1)
+		params = append(params, v)
+	}
+
+	rows, err := e.Db.Query(query, params...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var podcastIDs []string
+	for rows.Next() {
+		var id string
+		err := rows.Scan(&id)
+		if err != nil {
+			return nil, err
+		}
+		podcastIDs = append(podcastIDs, id)
+	}
+
+	return podcastIDs, nil
 }
--- storage/postgres/user_interactions.go
+++ storage/postgres/user_interactions.go
@@ -1,6 +1,9 @@
 package postgres
 
-import "database/sql"
+import (
+	"database/sql"
+	pb "discovery_service/genproto/user_interactions"
+)
 
 type UserInterRepo struct {
 	Db *sql.DB
@@ -8,4 +11,29 @@ type UserInterRepo struct {
 
 func NewUserInterRepo(db *sql.DB) *UserInterRepo {
 	return &UserInterRepo{Db: db}
+}
+
+func (u *UserInterRepo) DeleteLikeFromEpisodeOfPodcast(ids *pb.DeleteLike) (*pb.Success, error) {
+	query := `delete from user_interactions
+	where deleted_at = null and user_id = $1 and podcast_id = $2 and episode_id = $3`
+
+	tr, err := u.Db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err != nil {
+			tr.Rollback()
+		} else {
+			tr.Commit()
+		}
+	}()
+
+	_, err = tr.Exec(query, ids.UserId, ids.PodcastId, ids.EpisodeId)
+	if err != nil {
+		return &pb.Success{Success: false}, err
+	}
+
+	return &pb.Success{Success: true}, nil
 }
