aec19f43690d68306ca43dd46e5f6f73d77bd738
Ko'palov Muhammadjon <kupalovv.muhammadjon@gmail.com>
2024-07-02 10:20:23 +0500

    Submodule added and ternding & create service methods made

--- service/episode_metadata.go
+++ service/episode_metadata.go
@@ -4,18 +4,25 @@ import (
 	"context"
 	"database/sql"
 	"discovery_service/client"
+	pbc "discovery_service/genproto/comments"
 	pb "discovery_service/genproto/episode_metadata"
+	pbp "discovery_service/genproto/podcasts"
+
 	"discovery_service/storage/postgres"
 )
 
 type EpisodeMetadataService struct {
 	pb.UnimplementedEpisodeMetadataServer
-	Repo *postgres.EpisodeMetadataRepo
+	Repo          *postgres.EpisodeMetadataRepo
+	PodcastClient *pbp.PodcastsClient
+	CommentClient *pbc.CommentsClient
 }
 
-func NewEpisodeMetadataService(db *sql.DB) *EpisodeMetadataService {
+func NewEpisodeMetadataService(db *sql.DB, podcastClient *pbp.PodcastsClient, commentClient *pbc.CommentsClient) *EpisodeMetadataService {
 	return &EpisodeMetadataService{
 		Repo: postgres.NewEpisodeMetadataRepo(db),
+		PodcastClient: podcastClient,
+		CommentClient: commentClient,
 	}
 }
 
@@ -24,15 +31,44 @@ func (e *EpisodeMetadataService) GetPodcastsByGenre(ctx context.Context, req *pb
 	if err != nil {
 		return nil, err
 	}
-	
+
 	podcasts, _, err := client.RetrieveTitles(ids)
 	if err != nil {
 		return nil, err
 	}
 
 	// for _, v := range podcasts {
-		
+
 	// }
 
 	return nil, nil
+}
+
+func (e *EpisodeMetadataService) CreateEpisodeMetaData(ctx context.Context, episode *pb.EpisodeMetadata) (*pb.Void, error) {
+	err := e.Repo.CreateEpisodeMetaData(episode)
+
+	return &pb.Void{}, err
+}
+
+func (e *EpisodeMetadataService) GetTrendingPodcasts(context.Context, *pb.Void) (*pb.Podcasts, error) {
+	podcasts, err := e.Repo.GetTrendingPodcasts()
+	if err != nil {
+		return nil, err
+	}
+	for i := 0; i < len(podcasts.Podcasts); i++ {
+		p, err := (*e.PodcastClient).GetPodcastById(context.Background(), &pbp.ID{Id: podcasts.Podcasts[i].PodcastId})
+		if err != nil {
+			return nil, err
+		}
+
+		count, err := (*e.CommentClient).CountComments(context.Background(), &pbc.CountFilter{PodcastId: podcasts.Podcasts[i].PodcastId})
+		if err != nil {
+			return nil, err
+		}
+
+		podcasts.Podcasts[i].PodcastTitle = p.Title
+		podcasts.Podcasts[i].CommentCount = count.Count
+	}
+
+	return podcasts, nil
 }
